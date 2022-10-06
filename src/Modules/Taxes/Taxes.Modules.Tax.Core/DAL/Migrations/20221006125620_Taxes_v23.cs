3ca7996dfdf2ed97c9e26eb364ae52a9a22d0fcc
mkojoa <michael.ameyaw@persol.net>
2022-10-06 13:04:17 +0000

    ll

---
+++ src/Modules/Taxes/Taxes.Modules.Tax.Core/DAL/Migrations/20221006125620_Taxes_v23.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Taxes.Modules.Tax.Core.DAL.Migrations
+{
+    public partial class Taxes_v23 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CalculationRules",
+                schema: "taxes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalculationRules", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CalculationRules",
+                schema: "taxes");
+        }
+    }
+}
