397ff702163efd8af124f2e549dad5ec313c31d2
mkojoa <michael.ameyaw@persol.net>
2022-10-07 11:06:42 +0000

    minor update

--- src/Modules/Taxes/Taxes.Modules.Tax.Core/DAL/Migrations/20221007104830_Taxes_update_v01.Designer.cs
+++ src/Modules/Taxes/Taxes.Modules.Tax.Core/DAL/Migrations/20221007104830_Taxes_update_v01.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Taxes.Modules.Tax.Core.DAL;
 
@@ -11,9 +12,10 @@ using Taxes.Modules.Tax.Core.DAL;
 namespace Taxes.Modules.Tax.Core.DAL.Migrations
 {
     [DbContext(typeof(TaxesDbContext))]
-    partial class TaxesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221007104830_Taxes_update_v01")]
+    partial class Taxes_update_v01
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Modules/Taxes/Taxes.Modules.Tax.Core/DAL/Migrations/20221007104830_Taxes_update_v01.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Taxes.Modules.Tax.Core.DAL.Migrations
+{
+    public partial class Taxes_update_v01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                schema: "taxes",
+                table: "SavingsSchemes");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                schema: "taxes",
+                table: "SavingsSchemes");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                schema: "taxes",
+                table: "SavingsSchemes",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "CalcRule",
+                schema: "taxes",
+                table: "SavingsSchemes",
+                newName: "CalculationRuleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SavingsSchemes_CalculationRuleId",
+                schema: "taxes",
+                table: "SavingsSchemes",
+                column: "CalculationRuleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SavingsSchemes_CalculationRules_CalculationRuleId",
+                schema: "taxes",
+                table: "SavingsSchemes",
+                column: "CalculationRuleId",
+                principalSchema: "taxes",
+                principalTable: "CalculationRules",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SavingsSchemes_CalculationRules_CalculationRuleId",
+                schema: "taxes",
+                table: "SavingsSchemes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SavingsSchemes_CalculationRuleId",
+                schema: "taxes",
+                table: "SavingsSchemes");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                schema: "taxes",
+                table: "SavingsSchemes",
+                newName: "Description");
+
+            migrationBuilder.RenameColumn(
+                name: "CalculationRuleId",
+                schema: "taxes",
+                table: "SavingsSchemes",
+                newName: "CalcRule");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                schema: "taxes",
+                table: "SavingsSchemes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                schema: "taxes",
+                table: "SavingsSchemes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
